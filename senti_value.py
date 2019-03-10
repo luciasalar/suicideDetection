98529dfd87d9506261c37f1f1bc1dbcaa36f7590
abeeraldayel <aabeer@ksu.edu.sa>
2019-03-10 11:10:36 +0000

    updates

--- senti_value.py
+++ senti_value.py
@@ -87,19 +87,43 @@ def prepare_file_features(senti_list,userfile):
 
 
 if __name__ == '__main__':
+        
+        Crowd_users="C:/Users/Abeer/Dropbox/clpsych_workshop/Training_Testing/clpsych19_training_data/clpsych19_training_data/crowd_train.csv"
+        
+        #Testing_file="C:/Users/Abeer/Dropbox/clpsych_workshop/Training_Testing/testing_clpsych.csv"
+    
+        training_crowd_data = pd.read_csv(Crowd_users, sep=',', encoding='latin1', low_memory=False) 
+        users_ids=training_crowd_data["UserID"]
+        
+        #list of post of task B as provided by the organizers (no SW posts)
+        post_taskB= "C:/Users/Abeer/Dropbox/clpsych_workshop/Training_Testing/clpsych19_training_data/clpsych19_training_data/task_B_train.posts.csv"
+        Task_B_posts=pd.read_csv(post_taskB, sep=',', encoding='latin1', low_memory=False) 
+        
+        users_ids=Task_B_posts["user_id"]
+        
+        for users in users_ids:
+            Pos_ids_user=Task_B_posts.loc[(Task_B_posts["user_id"] == Task_B_posts) & (training_data['Stance'] == 'supporting') ]
+
+        #list of the posts texts for the users
+        
         userfile=pd.read_csv("C:/Users/Abeer/Dropbox/clpsych_workshop/Training_Testing/clpsych19_training_data/clpsych19_training_data/shared_task_posts.csv", sep=',', encoding='latin1', low_memory=False)        
         postslist=userfile['post_body']
-        #the sentiment of 10,48576 records 
-        list_senti=[]
-        for post in postslist:
-            if post is None:
-                list_senti.append("")
-            else:    
-                list_senti.append(RateSentiment(post))
-                
-                
-        #after analyzing the sentiment for each user we will save the new results in new user file
-        prepare_file_features(list_senti,userfile)
+        ##the sentiment of 10,48576 records 
+        
+        #loop over each user id in the main crowd user 
+        for user_id in users_ids:            
+            
+            #the sentiment of 10,48576 records 
+            list_senti=[]
+            for post in postslist:
+                if post is None:
+                    list_senti.append("")
+                else:    
+                    list_senti.append(RateSentiment(post))
+                    
+                    
+            #after analyzing the sentiment for each user we will save the new results in new user file
+            prepare_file_features(list_senti,userfile)
        
 
 #The above is OK for one text but inefficient to repeatedly call for many texts. Try instead: 
