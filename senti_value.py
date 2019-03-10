7bb302ba835ed7b5680216a18049433f01646b06
abeeraldayel <aabeer@ksu.edu.sa>
2019-03-10 11:43:52 +0000

    Update senti_value.py

--- senti_value.py
+++ senti_value.py
@@ -30,17 +30,17 @@ def RateSentiment(sentiString):
 #An example to illustrate calling the process.
 
 
-def prepare_file_features(senti_list,userfile):
+def prepare_file_features(list_senti,user,postslist):
     #="C:/Users/Abeer/Dropbox/clpsych_workshop/data_sample_clpsych19/user"+str(users_ids[i])+".posts.csv"
-    original_posts_df = pd.read_csv(userfile, sep=',',encoding='latin1')    
+    #original_posts_df = pd.read_csv(userfile, sep=',',encoding='latin1')    
     
-    new_CSV_file="C:/Users/Abeer/Dropbox/clpsych_workshop/data_sample_clpsych19/posts_sentiment.csv"
+    new_CSV_file="C:/Users/Abeer/Dropbox/clpsych_workshop/data_sample_clpsych19/posts_sentiment_"+str(user)+".csv"
     file_new= open(new_CSV_file,'w')
-    new_test_df = pd.read_csv(new_CSV_file, sep=',',encoding='latin1',names=["post_id",	"user_id",	"timestamp",	"subreddit"	,"post_title",	"post_body" , "sentiment"])    
+    new_test_df = pd.read_csv(new_CSV_file, sep=',',encoding='latin1',names=["post_id","sentiment"])    
     #convert timestamp to time
 
     index=0
-    for sentiments in senti_list:
+    for sentiments in list_senti:
         print(sentiments)
         sentiment_results=sentiments.split(" ")
         positive_strength=int(sentiment_results[0])
@@ -53,30 +53,16 @@ def prepare_file_features(senti_list,userfile):
         finalSentimint=""
         
         if positive_strength>negative_strength:
-            finalSentimint='pos'
+            finalSentimint='1'
             print("positive result "+ str(positive_strength) + "other "+str(negative_strength))
         elif positive_strength<negative_strength: 
-            finalSentimint='neg'
+            finalSentimint='-1'
             print("negative result "+ str(negative_strength))
         else:
-            finalSentimint='nut' #nutral
+            finalSentimint='0' #nutral
         
-        #post_id	user_id	timestamp	subreddit	post_title	post_body
-        post_id=original_posts_df.at[index,'post_id']
-        userID= original_posts_df.at[index,'user_id']
-        timestamp= original_posts_df.at[index,'timestamp'] 
-        subreddit= original_posts_df.at[index,'subreddit'] 
-        post_title= original_posts_df.at[index,'post_title'] 
-        post_body= original_posts_df.at[index,'post_body']
-                     
-       
         
-        new_test_df.loc[index, 'post_id'] = post_id
-        new_test_df.loc[index, 'user_id'] =userID
-        new_test_df.loc[index, 'timestamp'] = timestamp
-        new_test_df.loc[index, 'subreddit'] =subreddit
-        new_test_df.loc[index, 'post_title'] = post_title
-        new_test_df.loc[index, 'post_body'] =post_body
+        new_test_df.loc[index, 'post_id'] = postslist[index]
         new_test_df.loc[index, 'sentiment'] = finalSentimint
                    
         index=index+1
@@ -93,37 +79,34 @@ if __name__ == '__main__':
         #Testing_file="C:/Users/Abeer/Dropbox/clpsych_workshop/Training_Testing/testing_clpsych.csv"
     
         training_crowd_data = pd.read_csv(Crowd_users, sep=',', encoding='latin1', low_memory=False) 
-        users_ids=training_crowd_data["UserID"]
+        users_ids=training_crowd_data["user_id"]
         
         #list of post of task B as provided by the organizers (no SW posts)
         post_taskB= "C:/Users/Abeer/Dropbox/clpsych_workshop/Training_Testing/clpsych19_training_data/clpsych19_training_data/task_B_train.posts.csv"
         Task_B_posts=pd.read_csv(post_taskB, sep=',', encoding='latin1', low_memory=False) 
         
         users_ids=Task_B_posts["user_id"]
-        
-        for users in users_ids:
-            Pos_ids_user=Task_B_posts.loc[(Task_B_posts["user_id"] == Task_B_posts) & (training_data['Stance'] == 'supporting') ]
-
-        #list of the posts texts for the users
-        
         userfile=pd.read_csv("C:/Users/Abeer/Dropbox/clpsych_workshop/Training_Testing/clpsych19_training_data/clpsych19_training_data/shared_task_posts.csv", sep=',', encoding='latin1', low_memory=False)        
-        postslist=userfile['post_body']
-        ##the sentiment of 10,48576 records 
+        #postslist=userfile['post_id']
+        postslist=userfile
         
-        #loop over each user id in the main crowd user 
-        for user_id in users_ids:            
-            
-            #the sentiment of 10,48576 records 
+        for user in users_ids:
+            #list of sentiment for each user
             list_senti=[]
-            for post in postslist:
-                if post is None:
+            post_ids_user=Task_B_posts.loc[(Task_B_posts["user_id"] == user)]
+            post_ids_user=post_ids_user['post_id']
+            for post_id in post_ids_user:
+                #postslist=userfile['post_body']
+                postboday=postslist.loc[(postslist["post_id"] == post_id)]
+                postboday=postboday['post_body']
+                if postboday is None:
                     list_senti.append("")
                 else:    
-                    list_senti.append(RateSentiment(post))
+                    list_senti.append(RateSentiment(postboday))
                     
                     
             #after analyzing the sentiment for each user we will save the new results in new user file
-            prepare_file_features(list_senti,userfile)
+            prepare_file_features(list_senti,user,post_ids_user)
        
 
 #The above is OK for one text but inefficient to repeatedly call for many texts. Try instead: 
